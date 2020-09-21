f07de8d211f5d6e0db6bac88f775e0a1e8482859
maomao5959 <595971831@qq.com>
2020-09-21 16:12:11 +0800

    first commit

---
+++ 两个数组的交集/两个数组的交集.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let result=[];
+    for(let i=0;i<nums1.length;i++){
+        //通过index Of来判断循环的nums[i]在不在nums2里面，如果找到了。就在结果里面添加然后再nums2里面删除
+        let index=nums2.indexOf(nums1[i]);
+        if(index>-1){
+            result.push(nums1[i]);
+            nums2.splice(index,1)
+        }
+    }
+        return result;
+  };
