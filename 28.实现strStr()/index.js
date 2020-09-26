49a8042ac829f4dbc259c03ca167a6685ea21efe
maomao5959 <595971831@qq.com>
2020-09-26 16:53:14 +0800

    substring

--- 28.实现strStr()/index.js
+++ 28.实现strStr()/index.js
@@ -4,10 +4,14 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(needle==="") return 0;
+    if(needle===""){
+        return 0;
+    }
     for(let i=0;i<haystack.length;i++){
         if(haystack[i]===needle[0]){
-            if(haystack.substring(i,i+needle.length)===needle) return i;
+            if(haystack.substring(i,i+needle.length)===needle){
+                return i;
+            } 
         }
     }
     return -1;
