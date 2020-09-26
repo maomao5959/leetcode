a62c9e9b059cf7ffd276c4acc65741996aaf6b96
maomao5959 <595971831@qq.com>
2020-09-26 16:50:49 +0800

    双重for循环

--- 26.删除排序数组中的重复项/index.js
+++ 26.删除排序数组中的重复项/index.js
@@ -7,7 +7,7 @@ var removeDuplicates = function(nums) {
         for(let j=i+1;j<nums.length;j++){
             if(nums[i]==nums[j]){
                 nums.splice(j,1)
-                //关键是删除了一个数组项以后，下标要往前移动一位，所以要j--
+                //关键是删除一个数组项以后，下标要往前移动一位，所以要j--
                 j--
             }
         }
